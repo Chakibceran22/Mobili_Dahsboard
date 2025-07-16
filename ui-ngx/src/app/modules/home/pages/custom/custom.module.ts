1df7d34e881bf6e25299a0e1ab72a6732f197c00
MHND09 <131745373+MHND09@users.noreply.github.com>
2025-07-16 13:48:46 +0100

    Add AI chat feature

---
+++ ui-ngx/src/app/modules/home/pages/custom/custom.module.ts
@@ -0,0 +1,41 @@
+///
+/// Copyright © 2016-2025 The Thingsboard Authors
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
+import { CustomPageComponent } from './custom-page.component';
+import { CustomRoutingModule } from './custom-routing.module';
+import { HomeComponentsModule } from '@modules/home/components/home-components.module';
+import { AiChatService } from './ai-chat.service';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    CustomPageComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    HomeComponentsModule,
+    CustomRoutingModule
+  ],
+  providers: [
+    AiChatService
+  ]
+})
+export class CustomModule { }
